e229c2c3a006c06372cc1423b55f23ebbbf7c18f
AxelMoragaAlexandria <axelmoraga899@gmail.com>
2023-04-12 23:11:33 -0400

    Java Class 2

---
+++ src/Class10/E4D2Arrays.java
@@ -0,0 +1,35 @@
+package Class10;
+
+public class E4D2Arrays {
+    public static void main(String[] args) {
+
+/*
+whenever we repeat code we should replace it with
+a loop
+ */
+        int [][] matrix={
+                {10,20,30,},
+                {20,50,65},
+                {102,54,60}
+        };
+
+        for (int j = 0; j < 3; j++) {
+            //getting the complete oneD array from 2D array
+            int [] arr1=matrix[j];
+
+            for (int i = 0; i < arr1.length; i++) {
+                // going over all the elements from the oneD array
+                System.out.print(arr1[i]+" ");
+            }
+            // to print all rows on new line
+            System.out.println();
+        }
+
+
+
+
+
+
+
+    }
+}
--- src/Homework/Class9HW7.java
+++ src/Homework/Class9HW7.java
@@ -8,17 +8,15 @@ From an array of integer elements find the largest number.
  */
         int [] numbers={5,10,30,40,50,100,300,800};
 
+        int largestNumber=numbers[0];
 
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]>largestNumber){
+                largestNumber=numbers[i];
+            }
 
-
-
-
-
-
-
-
-
-
+        }
+        System.out.println(largestNumber);
 
 
 
